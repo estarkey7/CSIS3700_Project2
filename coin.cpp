18e9ee84775bb3591df904ae282a3e27bb3b9adc
estarkey7 <estarkey@keystarsystems.com>
2015-05-05 00:39:15 -0400

    Updated coin class image files and base score.

--- coin.cpp
+++ coin.cpp
@@ -8,13 +8,69 @@ namespace csis3700
 	coin::coin(string name_in, float initial_x, float initial_y, float sx_in, float sy_in, ALLEGRO_BITMAP *image, float move_speed_in) : enemy_sprite(name_in, initial_x, initial_y, sx_in, sy_in, image, move_speed_in)
 	{
 		cointBaseSequence = new image_sequence();
-		cointBaseSequence->add_image(image_library::get_instance()->get("coin1.png"), .3);
-		/*cointBaseSequence->add_image(image_library::get_instance()->get("bug_static2.png"), .3);
-		cointBaseSequence->add_image(image_library::get_instance()->get("bug_static3.png"), .3);
-		cointBaseSequence->add_image(image_library::get_instance()->get("bug_static2.png"), .3);
-		cointBaseSequence->add_image(image_library::get_instance()->get("bug_static1.png"), .3);*/
+		cointBaseSequence->add_image(image_library::get_instance()->get("coin1.png"), .1);
+		cointBaseSequence->add_image(image_library::get_instance()->get("coin2.png"), .1);
+		cointBaseSequence->add_image(image_library::get_instance()->get("coin3.png"), .1);
+		cointBaseSequence->add_image(image_library::get_instance()->get("coin4.png"), .1);
+		cointBaseSequence->add_image(image_library::get_instance()->get("coin5.png"), .1);
+		cointBaseSequence->add_image(image_library::get_instance()->get("coin6.png"), .1);
+		cointBaseSequence->add_image(image_library::get_instance()->get("coin7.png"), .1);
+		cointBaseSequence->add_image(image_library::get_instance()->get("coin6.png"), .1);
+		cointBaseSequence->add_image(image_library::get_instance()->get("coin5.png"), .1);
+
+		coinSpecialSequence->add_image(image_library::get_instance()->get("dollar1.png"), .1);
+		coinSpecialSequence->add_image(image_library::get_instance()->get("dollar2.png"), .1);
+		coinSpecialSequence->add_image(image_library::get_instance()->get("dollar3.png"), .1);
+		coinSpecialSequence->add_image(image_library::get_instance()->get("dollar4.png"), .1);
+		coinSpecialSequence->add_image(image_library::get_instance()->get("dollar5.png"), .1);
+		coinSpecialSequence->add_image(image_library::get_instance()->get("dollar6.png"), .1);
+		coinSpecialSequence->add_image(image_library::get_instance()->get("dollar7.png"), .1);
+		coinSpecialSequence->add_image(image_library::get_instance()->get("dollar6.png"), .1);
+		coinSpecialSequence->add_image(image_library::get_instance()->get("dollar5.png"), .1);
+
+		set_image_sequence(cointBaseSequence);
+		score_value = 10;
 	}
 
+
+	void coin::resolve(const collision& collision, sprite *other) 
+	{
+		// collision with ground
+		if (other->get_name() == "ground")
+		{
+
+
+			rectangle r = collision.collision_rectangle();
+			set_velocity(Vector2(get_velocity().get_x(), 0.0f));
+			set_position((get_position() + Vector2(0, -r.get_height() -20)));
+
+			// WHEN PLAYERS VELOCITY IS ALMOST 0(WITHIN FRICTION THRESHHOLD AMOUNT OF 0), SET IT TO EXACTLY 0;
+			//	IF ITS NOT CLOSE, APPLY FRICTION
+			if (get_velocity().get_x() > -frictionLimit && get_velocity().get_x() < frictionLimit)
+			{
+				//set_velocity(Vector2(get_velocity().get_x(), 0.0f));
+				//set_image_sequence(staticImageSequence);
+			}
+			else
+			{
+				//set_velocity(Vector2(get_velocity().get_x() * friction, 0.0f));
+			}
+
+			/*if (get_velocity().get_x() > frictionLimit)
+			{
+			set_image_sequence(walk_right_sequence);
+			al_play_sample_instance(walk_sound_instance);
+			}
+			else if (get_velocity().get_x() < -friction_threshhold)
+			{
+			set_image_sequence(walk_left_sequence);
+			al_play_sample_instance(walk_sound_instance);
+			}*/
+			
+		}
+		
+
+	}
 
 	coin::~coin()
 	{
--- coin.h
+++ coin.h
@@ -11,6 +11,8 @@ namespace csis3700
 		coin(string name_in, float initial_x, float initial_y, float sx_in, float sy_in, ALLEGRO_BITMAP *image, float move_speed_in);
 		image_sequence  *cointBaseSequence;
 		image_sequence  *coinSpecialSequence;
+
+		virtual void resolve(const collision& collision, sprite* other);
 		virtual ~coin();
 	};
 }
